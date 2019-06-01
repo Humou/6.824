2e42253fe81ee0134b953d3e58c9c4b97b18983c
humou <274301316@qq.com>
2019-06-01 15:10:30 +0800

    reslove lab1

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -14,15 +17,72 @@ import "fmt"
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
+	var wg sync.WaitGroup
 	switch phase {
 	case mapPhase:
 		ntasks = len(mapFiles)
 		n_other = nReduce
+		taskChan := make(chan DoTaskArgs, ntasks)
+		wg.Add(ntasks)
+		for i := 0; i < ntasks; i++{
+			args := DoTaskArgs{
+				JobName: jobName,
+				File: mapFiles[i],
+				Phase: mapPhase,
+				TaskNumber: i,
+				NumOtherPhase: n_other}
+				taskChan <- args
+		}
+
+		go func(){
+			wg.Wait()
+			close(taskChan)
+		}()
+		for args := range taskChan{
+			//fmt.Println(args.TaskNumber, "map")
+			arg := args
+			go func(){
+				workerAddr := <- registerChan
+				if ok := call(workerAddr, "Worker.DoTask", arg, nil); !ok{
+					taskChan <- arg
+				}else{
+					wg.Done()
+				}
+				registerChan <- workerAddr
+			}()
+		}
+
 	case reducePhase:
 		ntasks = nReduce
 		n_other = len(mapFiles)
+		taskChan := make(chan DoTaskArgs, ntasks)
+		wg.Add(ntasks)
+		for i := 0; i < ntasks; i++{
+			args := DoTaskArgs{
+				JobName: jobName,
+				File: mapFiles[i],
+				Phase: reducePhase,
+				TaskNumber: i,
+				NumOtherPhase: n_other}	
+			taskChan <- args
+		}
+		go func(){
+			wg.Wait()
+			close(taskChan)
+		}()
+		for args := range taskChan{
+			arg := args
+			go func(){
+				workerAddr := <- registerChan
+				if ok := call(workerAddr, "Worker.DoTask", arg, nil); !ok{
+					taskChan <- arg
+				}else{
+					wg.Done()
+				}
+				registerChan <- workerAddr
+			}()
+		}
 	}
-
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
@@ -30,5 +90,7 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+
 	fmt.Printf("Schedule: %v done\n", phase)
+	wg.Wait()
 }
